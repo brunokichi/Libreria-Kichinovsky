13765f1d9244123f0d264d2a9c70886d55fd095b
Bruno Kichinovsky <brunokichi@gmail.com>
2022-08-25 11:41:46 -0300

    Entrega

--- js/multicarousel.js
+++ js/multicarousel.js
@@ -16,3 +16,22 @@ $(document).ready(function(){
   $('#featureCarousel').carousel({interval: true});
   $('#featureCarousel').carousel('pause');
 });
+
+let itemsb = document.querySelectorAll('#MultiCb .carousel .carousel-item');
+itemsb.forEach((el) => {
+  const minPerSlide = 8
+  let next = el.nextElementSibling
+  for (var i=1; i<minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = itemsb[0]
+    }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+  }
+})
+$(document).ready(function(){
+  $('#featureCarousel').carousel({interval: true});
+  $('#featureCarousel').carousel('pause');
+});
